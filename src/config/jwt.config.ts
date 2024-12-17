0746da1b561cf916db645207bfe36a03f236a704
potato <potato.coderx@gmail.com>
2024-12-17 18:18:09 +0800

    ✨ feat: add swc

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,5 +1,7 @@
 import { registerAs, ConfigType } from '@nestjs/config';
 
-export const JwtConfig = registerAs('jwt', () => ({}));
+export const JwtConfig = registerAs('jwt', () => ({
+  secret: process.env.JWT_SECRET || 'potato-secret-key',
+}));
 
 export type IAppConfig = ConfigType<typeof JwtConfig>;
